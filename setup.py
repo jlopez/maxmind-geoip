2fd74fb574416eb2ad0c47ddefcc5849e23c0f17
Jesus Lopez <jesus@jesusla.com>
2011-07-13 16:52:10 -0700

    Updated setup.py to support MacPorts

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup, Extension
 module1 = Extension('GeoIP',
 	libraries = ['GeoIP'],
 	sources = ['py_GeoIP.c'],
-	library_dirs = ['/usr/local/lib'],
-	include_dirs = ['/usr/local/include'])
+	library_dirs = ['/opt/local/lib', '/usr/local/lib'],
+	include_dirs = ['/opt/local/include', '/usr/local/include'])
 
 setup (name = 'GeoIP-Python',
 	version = '1.2.4',
