4ec7259b2cc2c8902919947a5e2db5ac09586c73
Jesus Lopez <jesus@jesusla.com>
2011-07-13 16:38:24 -0700

    Initial commit - v1.2.4

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('GeoIP',
+	libraries = ['GeoIP'],
+	sources = ['py_GeoIP.c'],
+	library_dirs = ['/usr/local/lib'],
+	include_dirs = ['/usr/local/include'])
+
+setup (name = 'GeoIP-Python',
+	version = '1.2.4',
+	description = 'This is a python wrapper to GeoIP',
+	ext_modules = [module1])
